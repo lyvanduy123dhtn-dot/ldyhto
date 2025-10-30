8e1e2c301dc584667f230414931db2941fc3f4ba
lyvanduy123dhtn-dot <lyvanduy123dhtn@gmail.com>
2025-10-30 22:43:25 +0700

    bsa1911.py

--- bsa1911.py
+++ bsa1911.py
@@ -8,9 +8,9 @@ import json,requests,time
 from time import strftime
 from pystyle import Colorate, Colors, Write, Add, Center
 ZALO = 'https://zalo.me/g/ehnpay312'
-ADMIN = 'LDYH'
+ADMIN = 'LDYHTOOL'
 VERSION = '1.0'
-NHV = '\033[1;91m[\033[1;92m●\033[1;91m]\033[1;97m ➻❥'  
+LVD = '\033[1;91m[\033[1;92m●\033[1;91m]\033[1;97m ➻❥'  
 def banner():
     print(f''' 
 Tool Share Cookie Max Speed
@@ -97,124 +97,4 @@ while True:
         main_share()
     except KeyboardInterrupt:
         print('\n\033[38;5;245m[\033[38;5;9m!\033[38;5;245m] \033[38;5;9mNhớ Đăng Ký Kênh C25 Tool Nhé^^\033[0m')
-        sys.exit()print (Colorate.Diagonal(Colors.blue_to_white, "║Tool Tiện Ích   ║"))
-print (Colorate.Diagonal(Colors.blue_to_white, "╚════════════════╝"))
-print("\033[1;31m[\033[1;37m<>\033[1;31m] \033[1;37m=> \033[1;35mNhập\033[1;36m Số \033[1;31m[\033[1;33m8.1\033[1;31m] \033[1;35mTOOL DOSS WEB & IP")
-print("\033[1;31m[\033[1;37m<>\033[1;31m] \033[1;37m=> \033[1;35mNhập\033[1;36m Số \033[1;31m[\033[1;33m8.2\033[1;31m] \033[1;35mTOOL SPAM SMS")
-print("\033[1;31m[\033[1;37m<>\033[1;31m] \033[1;37m=> \033[1;35mNhập\033[1;36m Số \033[1;31m[\033[1;33m8.3\033[1;31m] \033[1;35mTOOL BUFF TIKTOK ")
-print("\033[1;31m[\033[1;37m<>\033[1;31m] \033[1;37m=> \033[1;35mNhập\033[1;36m Số \033[1;31m[\033[1;33m8.4\033[1;31m] \033[1;35mTOOL REG ACC FB")
-print("\033[1;31m[\033[1;37m<>\033[1;31m] \033[1;37m=> \033[1;35mNhập\033[1;36m Số \033[1;31m[\033[1;33m8.5\033[1;31m] \033[1;35mTOOL REG PAGE PR5")
-print("\033[1;31m[\033[1;37m<>\033[1;31m] \033[1;37m=> \033[1;35mNhập\033[1;36m Số \033[1;31m[\033[1;33m8.6\033[1;31m] \033[1;35mTOOL REG ACC TTC")
-
-print("\033[1;31m────────────────────────────────────────────────────────────")
-print (Colorate.Diagonal(Colors.blue_to_white, "╔════════════════╗"))
-print (Colorate.Diagonal(Colors.blue_to_white, "║    Tool DEMO   ║"))
-print (Colorate.Diagonal(Colors.blue_to_white, "╚════════════════╝"))
-print("\033[1;31m[\033[1;37m<>\033[1;31m] \033[1;37m=> \033[1;35mNhập\033[1;36m Số \033[1;31m[\033[1;33m9.1\033[1;31m] \033[1;35mTOOL DEMO")
-print("\033[1;31m[\033[1;37m<>\033[1;31m] \033[1;37m=> \033[1;35mNhập\033[1;36m Số \033[1;31m[\033[1;33m00\033[1;31m] '\033[1;39mTHOÁT TOOL")
-
-print("\033[1;31m────────────────────────────────────────────────────────────")
-
-chon = str(input('\033[1;31m[\033[1;37m<>\033[1;31m] \033[1;37m=> \033[1;35mNhập\033[1;36m Số \033[1;37m: \033[1;33m'))
-
-
-# -----------------------------------------------------------
-# PHẦN SỬA CHỮA CHÍNH BẮT ĐẦU TỪ ĐÂY
-# Tất cả 'if' đã được đổi thành 'elif' để tạo thành một khối menu
-# -----------------------------------------------------------
-## xin phep khanh duy su dung chuc nang thoat tool
-if chon == '00' :
-    exec(requests.get('https://raw.githubusercontent.com/Khanh23047/thoattool/main/.github/workflows/main.yml').text)
-    #tool tđs
-elif chon == '1.1' :
-    exec(requests.get('https://raw.githubusercontent.com/lyvanduy123dhtn-dot/ldyhto/main/td7272stt.py').text)
-elif chon == '1.2':
-    exec(requests.get('https://raw.githubusercontent.com/lyvanduy123dhtn-dot/ldyhto/main/td7272stt.py').text)
-elif chon == '1.3' :
-    exec(requests.get('https://raw.githubusercontent.com/lyvanduy123dhtn-dot/ldyhto/main/tdsfb.py').text) 
-elif chon == '1.4' :
-    exec(requests.get('https://raw.githubusercontent.com/lyvanduy123dhtn-dot/ldyhto/main/109tdsig1990.py').text) 
-elif chon == '1.00' : 
- exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Mktds/main/4.py').text) 
- #tool spam sms
-elif chon == '1.5' :
-    exec(requests.get('https://raw.githubusercontent.com/lyvanduy123dhtn-dot/ldyhto/main/ttcfb.py').text)
-elif chon == '1.6' :
-    exec(requests.get('https://raw.githubusercontent.com/lyvanduy123dhtn-dot/ldyhto/main/ttctt.py').text)
-    #tool đào mail
-elif chon == '3.1' :
- exec(requests.get('https://raw.githubusercontent.com/lyvanduy123dhtn-dot/ldyhto/main/scanaolayma81819jsj.py').text)
-elif chon == '3.2' :
- exec(requests.get('https://raw.githubusercontent.com/lyvanduy123dhtn-dot/ldyhto/main/getprxy8182zn9ak.py').text)
-elif chon == '3.3' :
-    exec(requests.get('https://raw.githubusercontent.com/lyvanduy123dhtn-dot/ldyhto/main/locproxy9181.py').text)
-elif chon == '3.4' :
-    exec(requests.get('https://raw.githubusercontent.com/lyvanduy123dhtn-dot/ldyhto/main/regalectiffincrane.py').text)
-elif chon == '3.5' :
-    exec(requests.get('https://raw.githubusercontent.com/lyvanduy123dhtn-dot/ldyhto/main/regarena1919.py').text)
-    #tool đào&check proxy
-elif chon == '4.1' :
-    exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Checklivedieproxy/main/p.py').text)
-elif chon == '4.2' :
-    exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Check-live-die-DEMO/main/q.py').text)
-elif chon == '4.3' :
-    exec(requests.get('https://raw.githubusercontent.com/Khanh23047/ChecklivedieDEMO/main/p.py').text)
-elif chon == '4.4' :
-    exec(requests.get('https://raw.githubusercontent.com/Khanh23047/DaoprxDEMO/main/daoprxDEMO.py').text)
-elif chon == '4.5' :
-    exec(requests.get('https://raw.githubusercontent.com/Khanh23047/DaoprxDEMO/main/p.py').text)
-elif chon == '4.6' :
-  exec(requests.get('https://raw.githubusercontent.com/Khanh23047/DaoproxyDEMO/main/p.py').text)
-elif chon == '4.7' :   
-   exec(requests.get('https://raw.githubusercontent.com/Khanh23047/DaoproxyDEMO/main/p.py').text)
-elif chon == '4.8' :  
-    exec(requests.get('https://raw.githubusercontent.com/Khanh23047/DaoproxyDEMOvip/main/p.py').text)
-    #tool en&dec
-elif chon == '5.1' :
-    exec(requests.get('https://raw.githubusercontent.com/nguyenxuantrinhnotpd/Shenron/refs/heads/main/shenron.py').text)
-elif chon == '5.2' :
-     exec(requests.get('https://raw.githubusercontent.com/KhanhNguyen9872/kramer-specter_deobf/main/kramer-specter-deobf.py').text)
-   #tool golike
-elif chon == '6.1' :  
-    exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Golike/main/golike.py').text)
-elif chon == '6.2' :
-    exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Golike-ig/main/p.py').text)
-elif chon == '6.3' :
-     exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Golike-Twitter-/main/p.py').text)
-     #tool idol khác 
-elif chon == '7.1' :
-    exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Tool-vlong/main/p.py').text)
-elif chon == '7.2' :
-    exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Tool-trinh-huong/main/huong.py').text)
-elif chon == '7.3' :
-	exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Full-mail/main/vietcode_toolmeow.py').text)
-elif chon == '7.4' :
-  exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Tool-hdt/main/p.py').text)
-elif chon == '7.5' :   
-   exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Tool-lkz/main/p.py').text)
-elif chon == '7.6' :  
-    exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Tool-jray/main/haha.py').text)
-elif chon == '7.7' :  
-    exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Beta-tool/main/beta.py').text)
-    #tool tiện ích
-elif chon == '8.1' :
-    exec(requests.get('https://raw.githubusercontent.com/lyvanduy123dhtn-dot/ldyhto/main/doss.py').text)
-elif chon == '8.2' :
-     exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Reg-pro5-vip/main/reg.py').text)
-elif chon == '8.3' :
-    exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Rutgonlink/main/10.py').text)
-elif chon == '8.4' :
-    exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Phanhoilink/main/10.py').text)
-elif chon == '8.5' :
-	exec(requests.get('https://raw.githubusercontent.com/Khanh23047/L-c-Link-T-File/main/10.py').text)
-elif chon == '8.6' :
-  exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Reg-fb/main/10.py').text)
-  #tool tiện ích 
-elif chon == '9.1' :   
-   exec(requests.get('https://raw.githubusercontent.com/Khanh23047/May-tinh/main/0.py').text)
-else :
-    # Khối 'else' này sẽ bắt bất kỳ lựa chọn nào không có trong menu
-    print(f"\n{do}[!] Lựa chọn '{chon}' không hợp lệ. Vui lòng chạy lại tool và chọn đúng!{end}")
-    sleep(2)
-    exit()
-
+        sys.exit()
